8c3d5f2679851845c4afc3aa9ce10c781e4695a9
Sebastian Reger <swreger@mix.wvu.edu>
2018-09-17 15:08:20 -0400

    Assignment 3

---
+++ DisplayImage/src/DisplayImage.cpp
@@ -0,0 +1,36 @@
+// Copyright [2018] <Sebastian Reger>
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using cv::Mat;
+using cv::imread;
+using cv::namedWindow;
+using cv::WINDOW_AUTOSIZE;
+using cv::waitKey;
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+     return -1;
+    }
+
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);     // Read the file
+
+    if (!image.data) {     // Check for invalid input
+        cout <<  "Could not open or find the image" << std::endl;
+        return -1;
+    }
+
+    namedWindow("Display window", WINDOW_AUTOSIZE);
+        // Create a window for display.
+    imshow("Display window", image);     // Show our image inside it.
+
+    waitKey(0);     // Wait for a keystroke in the window
+    return 0;
+}
